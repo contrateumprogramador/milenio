2fdd751f8683b58f5c595cd592f307480f7cc1e9
Bruno Marra <brunomarram@hotmail.com>
2021-02-18 15:13:36 -0300

    fix: finish first details of front opinions

--- loja/app/components/li-opinions/li-opinions.directive.js
+++ loja/app/components/li-opinions/li-opinions.directive.js
@@ -7,11 +7,15 @@ module.exports = function(ngModule) {
             template: require("./li-opinions.view.html"),
             replace: true,
             scope: {
-                items: "="
+                opinions: "=",
+                productId: "="
             },
             controllerAs: "vm",
-            controller: function() {
+            controller: function($scope, $mdDialog, Loja, toast) {
                 var vm = this;
+
+                vm.productId = $scope.productId
+                vm.user = Loja.Auth.me;
 
                 vm.grades = [
                     { title: "Média", value: "avg" },
@@ -55,10 +59,58 @@ module.exports = function(ngModule) {
 
                 //Methods
                 vm.starFilled = starFilled
+                vm.opinionDialog = opinionDialog
 
                 //Functions
                 function starFilled(current, opinion, key) {
                     return (current+1) <= opinion[key]
+                }
+
+                function opinionDialog(ev) {        
+                    $mdDialog.show({
+                        controller: function($mdDialog, Loja) {
+                            var ctrl = this;
+
+                            ctrl.form = {
+                                costBenefit: 1,
+                                quality: 1,
+                                characteristics: 1,
+                                productId: vm.productId
+                            }
+
+                            //Methods
+                            ctrl.cancel = cancel
+                            ctrl.select = select
+                            ctrl.starFilled = starFilled
+                            ctrl.submit = submit
+                            
+                            //Functions
+                            function cancel(){
+                                $mdDialog.cancel();
+                            };
+
+                            function select(index, key) {
+                                ctrl.form[key] = index+1
+                            }
+
+                            function starFilled(index, key) {
+                                return (index+1) <= ctrl.form[key]
+                            }
+
+                            function submit() {
+                                $mdDialog.hide("Opinião registrada com sucesso");
+                            }
+                        },
+                        controllerAs: 'ctrl',
+                        template: require('../../components/li-opinions/li-opinions-dialog.view.html'),
+                        parent: angular.element(document.body),
+                        targetEvent: ev,
+                        clickOutsideToClose: false,
+                        fullscreen: true
+                    })
+                    .then(function(answer) {
+                        toast.message(answer)
+                    });
                 }
             }
         };
