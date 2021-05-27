37905a76a356f28ddaa568af4aab155a7bfee673
Mario Sergio <marionbxcz@gmail.com>
2021-05-26 22:52:03 -0300

    feat: create dialog component for product pictures

--- loja/app/components/li-product-carousel/li-product-carousel.directive.js
+++ loja/app/components/li-product-carousel/li-product-carousel.directive.js
@@ -15,7 +15,32 @@ module.exports = function(ngModule) {
                 var vm = this;
 
                 vm.item = $scope.item;
+                vm.index = 0;
                 vm.insideDialog = $scope.inside;
+
+                $scope.openDialog = () => {
+                    const dialog = document.getElementById('dialog')
+                    const carouselDialog = document.getElementById('carousel-dialog')
+
+                    while (carouselDialog.firstChild) dialog.appendChild(carouselDialog.firstChild)
+                    dialog.style.zIndex = 1000
+                }
+
+                $scope.closeDialog = () => {
+                    const dialog = document.getElementById('dialog')
+                    const carouselDialog = document.getElementById('carousel-dialog')
+
+                    while (dialog.firstChild) carouselDialog.appendChild(dialog.firstChild)
+                    dialog.style.zIndex = -1
+                }
+
+                $scope.previousPicture = () => {
+                    if (vm.index > 0) vm.index -= 1
+                }
+
+                $scope.nextPicture = () => {
+                    if (vm.index < vm.item.pictures.length) vm.index += 1
+                }
             }
         };
     });
