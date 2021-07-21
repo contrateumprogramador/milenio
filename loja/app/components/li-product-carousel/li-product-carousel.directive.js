d4e1256900d4a564b7b80445b5d78aa9b00d6e74
vitor hugo <vho2009@hotmail.com>
2021-07-21 19:10:37 -0300

    Adicionando pinch-zoom nas imagens e modificando o ref no orcamento enviado por email

--- loja/app/components/li-product-carousel/li-product-carousel.directive.js
+++ loja/app/components/li-product-carousel/li-product-carousel.directive.js
@@ -1,7 +1,9 @@
-module.exports = function(ngModule) {
+module.exports = function (ngModule) {
     require("./li-product-carousel.sass");
 
-    ngModule.directive("liProductCarousel", function() {
+    ngModule.directive("liProductCarousel", function () {
+        var pinchZoom = require('pinch-zoom');
+
         return {
             restrict: "EA",
             template: require("./li-product-carousel.view.html"),
@@ -11,7 +13,7 @@ module.exports = function(ngModule) {
                 inside: "="
             },
             controllerAs: "vm",
-            controller: function($scope) {
+            controller: function ($scope) {
                 var vm = this;
 
                 vm.item = $scope.item;
@@ -19,28 +21,33 @@ module.exports = function(ngModule) {
                 vm.insideDialog = $scope.inside;
 
                 $scope.openDialog = () => {
+                    var el = document.querySelector('.wrapper');
+                    var pzoom = pinchZoom(el);
+
                     const dialog = document.getElementById('dialog')
                     const carouselDialog = document.getElementById('carousel-dialog')
 
                     while (carouselDialog.firstChild) dialog.appendChild(carouselDialog.firstChild)
-                    dialog.style.zIndex = 1000
-                }
+                    dialog.style.zIndex = 1000;
+                };
 
                 $scope.closeDialog = () => {
                     const dialog = document.getElementById('dialog')
                     const carouselDialog = document.getElementById('carousel-dialog')
 
                     while (dialog.firstChild) carouselDialog.appendChild(dialog.firstChild)
-                    dialog.style.zIndex = -1
-                }
+                    dialog.style.zIndex = -1;
+                };
 
                 $scope.previousPicture = () => {
-                    if (vm.index > 0) vm.index -= 1
-                }
+                    if (vm.index > 0) vm.index -= 1;
+                };
 
                 $scope.nextPicture = () => {
-                    if (vm.index < vm.item.pictures.length) vm.index += 1
-                }
+                    if (vm.index < vm.item.pictures.length) vm.index += 1;
+                };
+
+
             }
         };
     });
